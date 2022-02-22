cb79e826d117184e033f29c2e2bb4b9afbdda945
jinsheng zhao <marlowevskyzhao@gmail.com>
2022-02-22 11:58:02 +0100

    validation done

--- app/models/politician.rb
+++ app/models/politician.rb
@@ -1,2 +1,8 @@
 class Politician < ApplicationRecord
+  has_many :bookmarks, dependent: :destroy
+  has_many :ratings, dependent: :destroy
+  has_many :bookings, dependent: :destroy
+  has_many :availabilities, dependent: :destroy
+
+  validates :full_name, presence: true
 end
