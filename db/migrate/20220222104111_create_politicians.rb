8d987d7062e8be1e2f345291eea83d0e8f01316b
jinsheng zhao <marlowevskyzhao@gmail.com>
2022-02-22 11:42:32 +0100

    politician model made

---
+++ db/migrate/20220222104111_create_politicians.rb
@@ -0,0 +1,16 @@
+class CreatePoliticians < ActiveRecord::Migration[6.1]
+  def change
+    create_table :politicians do |t|
+      t.string :full_name
+      t.integer :age
+      t.text :biography
+      t.string :expertise
+      t.string :skills
+      t.integer :reach
+      t.string :location
+      t.integer :price
+
+      t.timestamps
+    end
+  end
+end
