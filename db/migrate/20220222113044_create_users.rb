aa9d661e48b2d80306dc871358181a6dd485e199
jinsheng zhao <marlowevskyzhao@gmail.com>
2022-02-22 12:35:14 +0100

    user model created

---
+++ db/migrate/20220222113044_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :full_name
+      t.string :organization_name
+      t.string :sector
+      t.string :location
+      t.string :email
+      t.string :password
+
+      t.timestamps
+    end
+  end
+end
