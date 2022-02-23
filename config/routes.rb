8c1c15fd8f1da1c73f915cda54c27fcf462ae8e4
Quoc Hanhoan Truong <tqh@posteo.net>
2022-02-23 16:06:23 +0100

    ssd

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get "/pages/congratulations", to: "pages#index", as: :congratulations
   resources :politicians, only: [:index, :show] do
     resources :availabilities, only: [:show]
   end
