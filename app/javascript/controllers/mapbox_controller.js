e421b15b1475b8a7af99b39f6ef403ffab1f7ca7
Quoc Hanhoan Truong <tqh@posteo.net>
2022-02-25 12:46:06 +0100

    draftmap

--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -1,5 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 import mapboxgl from "mapbox-gl"
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder'
+import 'mapbox-gl/dist/mapbox-gl.css'
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiYWxvaGFubnUiLCJhIjoiY2wwMTc1cWs2MHJqMDNwcXBmb3NpN21xeSJ9.la0-gHyLW1ym7FIPRYrmZQ';
 const map = new mapboxgl.Map({
@@ -26,23 +28,23 @@ export default class extends Controller {
   }
 }
 
-// #addMarkersToMap() {
-//   this.markersValue.forEach((marker) => {
-//     new mapboxgl.Marker()
-//       .setLngLat([ marker.lng, marker.lat ])
-//       .addTo(this.map)
-//   });
-// }
+#addMarkersToMap() {
+  this.markersValue.forEach((marker) => {
+    new mapboxgl.Marker()
+      .setLngLat([ marker.lng, marker.lat ])
+      .addTo(this.map)
+  });
+}
 
-// connect() {
-//   // [...]
-//   this.#addMarkersToMap()
-//   this.#fitMapToMarkers()
-// }
+connect() {
+  // [...]
+  this.#addMarkersToMap()
+  this.#fitMapToMarkers()
+}
 
-// // [...]
-// #fitMapToMarkers() {
-//   const bounds = new mapboxgl.LngLatBounds()
-//   this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
-//   this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
-// }
+// [...]
+#fitMapToMarkers() {
+  const bounds = new mapboxgl.LngLatBounds()
+  this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
+  this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
+}
