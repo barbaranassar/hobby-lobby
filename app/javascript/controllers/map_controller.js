234546cbabd99e2528883ea48facf444bfbd1592
Barbara Nassar Lobato de Almeida <ba.almeida@hotmail.com>
2022-09-04 12:58:14 +0200

    styled mapbox

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -20,10 +20,10 @@ export default class extends Controller {
 
   #addMarkersToMap() {
     this.markersValue.forEach((marker) => {
-      const popup = new mapboxgl.Popup().setHTML(marker.info_window) // Add this
+      const popup = new mapboxgl.Popup().setHTML(marker.info_window)
       new mapboxgl.Marker()
         .setLngLat([ marker.lng, marker.lat ])
-        .setPopup(popup) // Add this
+        .setPopup(popup)
         .addTo(this.map)
     });
   }
